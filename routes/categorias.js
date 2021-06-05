6a6e61e6e1a4d7211cec0a588a73bca5b3973c0c
pamelanavarroha <pamela.navarroha@gmail.com>
2021-06-05 00:16:44 -0500

    completando CRUD en routes/categorias

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+
 const {
-    consultarCategorias,
+    consultarCategorias, 
+    crearCategoria, 
+    consultarCategoria, //Completando estructura del CRUD
+    modificarCategoria,
+    eliminarCategoria
 } = require('../controllers/categorias')
 
 router.get("/", consultarCategorias);
+router.post("/crear-categoria", crearCategoria);
+router.get("/consultar-categoria/:id", consultarCategoria);
+router.put("/modificar-categoria/:id", modificarCategoria);
+router.delete("/eliminar-categoria/:id", eliminarCategoria);
 
 module.exports = router;
