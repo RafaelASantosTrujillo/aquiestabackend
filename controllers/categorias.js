e80a2fe3a53e6e5a893b9dae0dae57df10226664
pamelanavarroha <pamela.navarroha@gmail.com>
2021-06-05 00:04:15 -0500

    completando CRUD en controllers/categorias

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -12,6 +12,115 @@ function consultarCategorias(req, res) {
         });
 }
 
+// Función para crear Categoría
+function crearCategoria(req, res, next) {
+    const ctg = Categoria.build(req.body);
+    ctg.save()
+        .then((categoria) => {
+            console.log("Nueva categoria añadida");
+            return res.sendStatus(201).json(categoria);
+        })
+        .catch(next);
+}
+
+// Función para consultar una categoría
+function consultarCategoria(req, res) {
+    Categoria.findOne({
+        where: {
+            idCategoria: req.params.id,
+        },
+    })
+        .then((categoria) => {
+            console.log("Entra a consultar categoria");
+            if (categoria) {
+                return res.json(categoria);
+            } else {
+                return res.sendStatus(404);
+            }
+        })
+        .catch((error) => {
+            console.log("¡Ups! Ocurrió el siguiente error:", error);
+            return res.sendStatus(404);
+        });
+}
+
+// Función para modificar categoría
+function modificarCategoria(req, res) {
+    Categoria.findOne({
+        where: {
+            idCategoria: req.params.id,
+        },
+    })
+        .then((CategoriaInfo) => {
+            if (CategoriaInfo) {
+                Categoria.update(
+                    { nombreCategoria: req.body.nombreCategoria },
+                    {
+                        where: {
+                            idCategoria: req.params.id,
+                        },
+                    }
+                )
+                    .then((categoria) => {
+                        console.log("Categoría actualizada");
+                        return res.sendStatus(200).json(categoria);
+                    })
+                    .catch((err) => {
+                        console.log(
+                            "Error al modificar categoria: ",
+                            err
+                        );
+                        return res.sendStatus(500);
+                    });
+            } else {
+                return res.sendStatus(404);
+            }
+        })
+        .catch((error) => {
+            console.log("Error al intentar modificar categoria", error);
+            res.sendStatus(500);
+        });
+}
+
+// Función para eliminar una categoría
+function eliminarCategoria(req, res) {
+    Categoria.findOne({
+        where: {
+            idCategoria: req.params.id,
+        },
+    })
+        .then((CategoriaInfo) => {
+            if (CategoriaInfo) {
+                Categoria.destroy({
+                    where: {
+                        idCategoria: req.params.id,
+                    },
+                })
+                    .then((categoria) => {
+                        console.log("Categoría eliminada");
+                        return res.sendStatus(200);
+                    })
+                    .catch((err) => {
+                        console.log(
+                            "Error al eliminar categoría: ",
+                            err
+                        );
+                        return res.sendStatus(500);
+                    });
+            } else {
+                return res.sendStatus(404);
+            }
+        })
+        .catch((error) => {
+            console.log("Error al intentar eliminar categoria", error);
+            res.sendStatus(500);
+        });
+}
+
 module.exports = {
     consultarCategorias,
+    crearCategoria,
+    consultarCategoria,
+    modificarCategoria,
+    eliminarCategoria
 }
